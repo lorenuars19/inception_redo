e58d4c34e97e60533700be869b8bf7651e0398db
lorenuar <lorenuar.s19@gmail.com>
2022-07-18 16:09:08 +0200

    fix ?

--- srcs/wordpress/wp_setup.sh
+++ srcs/wordpress/wp_setup.sh
@@ -3,7 +3,7 @@ mkdir -p /www/wordpress
 wp core download --path=/www/wordpress --allow-root --locale=en_US
 
 echo =========================================================================== WP CONFIG
-wp core config --path=/www/wordpress --allow-root --url=localhost --dbhost=mariadb --dbname=wordpress --dbuser=${WP_DB_USER} --dbpass=${WP_DB_PSWD}
+wp core config --path=/www/wordpress --allow-root --url=localhost --dbhost=mysql --dbname=wordpress --dbuser=${WP_DB_USER} --dbpass=${WP_DB_PSWD}
 chmod 744 ./wordpress/wp-config.php
 echo =========================================================================== WP INSTALL
 wp core install --path=/www/wordpress --allow-root --url=lorenuar.42.fr --title="Title" --admin_name=lorenuar --admin_password=${WP_ADMIN_PSWD} --admin_email=lorenuar@lorenuar.42.fr
