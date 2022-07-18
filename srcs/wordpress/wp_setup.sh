68f5546b076f7d9ff0d6c4dcf35da8ded87519ee
lorenuar <lorenuar.s19@gmail.com>
2022-07-18 15:34:13 +0200

    add: www.conf COPY

--- srcs/wordpress/wp_setup.sh
+++ srcs/wordpress/wp_setup.sh
@@ -10,8 +10,6 @@ wp core install --path=/www/wordpress --allow-root --url=lorenuar.42.fr --title=
 
 wp user create --path=/www/wordpress --allow-root editor editor@lorenuar.42.fr --role='editor' --user_pass=${WP_EDIT_PSWD}
 
-# sed -i "/s/listen = .*/listen = nginx:9000/g" /etc/php8/php-fpm.d/www.conf
-
 chown -R www:www /www/wordpress
 
 /usr/sbin/php-fpm8 --nodaemonize
